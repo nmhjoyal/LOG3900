4ff3c9985000ecc520c4b5d7b340b82b14a2ec40
Zakari Rahal <zakari.rahal@polymtl.ca>
2020-01-25 15:55:25 -0500

    C# sockets and NodeJS

---
+++ Server/Client/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text; 
+public class Client {  
+    public static void Main(String[] args) {  
+        byte[] bytes = new byte[1024];  
+        Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        System.Net.IPEndPoint remoteEP = new IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 5000);
+        sender.Connect(remoteEP);  
+        Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());  
+        // Encode the data string into a byte array.    
+        // byte[] msg = Encoding.ASCII.GetBytes("abcdef");
+        string msg = "abcdef";
+        // Send the data through the socket.    
+        int bytesSent = sender.Send(msg);
+        // Receive the response from the remote device.
+        int bytesRec = sender.Receive(bytes);   
+        // Release the socket.    
+        sender.Shutdown(SocketShutdown.Both);  
+        sender.Close();  
+    }
+}
--- Server/Server.js
+++ Server/Server.js
@@ -1,8 +1,32 @@
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    console.log("req : " + req);
+    if(req.url === '/'){
+        res.write("Hi");
+        res.end();
+    }
+});
 
 server.on('connection', (socket) => {
-    console.log('New connection');
+    console.log(socket);
 });
 
 server.listen(5000);
+
+/*
+socket.on('data',function(data){
+    var bread = socket.bytesRead;
+    var bwrite = socket.bytesWritten;
+    console.log('Bytes read : ' + bread);
+    console.log('Bytes written : ' + bwrite);
+    console.log('Data sent to server : ' + data);
+  
+    //echo data
+    var is_kernel_buffer_full = socket.write('Data ::' + data);
+    if(is_kernel_buffer_full){
+      console.log('Data was flushed successfully from kernel buffer i.e written successfully!');
+    }else{
+      socket.pause();
+    }
+});
+*/
