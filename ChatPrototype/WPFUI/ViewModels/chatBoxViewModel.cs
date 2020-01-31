09d21569aecb61d8652e4b29a8a007bfd24a9609
Emilio Gagnon <emiliogagnonpro@gmail.com>
2020-01-31 01:52:13 -0500

    [EG] Scrollbar added to chatbox (invisible)

--- ChatPrototype/WPFUI/ViewModels/chatBoxViewModel.cs
+++ ChatPrototype/WPFUI/ViewModels/chatBoxViewModel.cs
@@ -36,6 +36,7 @@ namespace WPFUI.ViewModels
             if (currentMessage != null & currentMessage != "")
             {
                 messages.Add(new MessageModel(currentMessage, _userData.userName, DateTime.Now));
+                currentMessage = "";
             }
 
         }
@@ -47,6 +48,12 @@ namespace WPFUI.ViewModels
             _messages = new BindableCollection<MessageModel>();
             messages.Add(new MessageModel("this is a test message", "Barack Obama", DateTime.Now));
             messages.Add(new MessageModel("this is another test message", "Kobe", DateTime.Now));
+            messages.Add(new MessageModel("cette architecture est a chier", "Oliver G.", DateTime.Now));
+            messages.Add(new MessageModel("WOW c'est vrm beau mtnt ! on va manger ?", "Karima", DateTime.Now));
+            messages.Add(new MessageModel("non jte donnerais par leur code !!", "Amar", DateTime.Now));
+            messages.Add(new MessageModel("yo le serveur c'est chaud", "Hubert", DateTime.Now));
+            messages.Add(new MessageModel("merde j'aurais du prendre le lien avec le client leger", "Zackary", DateTime.Now));
+            messages.Add(new MessageModel("qui a fait une activite de 5hr ? Inacceptable", "Sebastien C.", DateTime.Now));
             _userData = userdata;
         }
 
