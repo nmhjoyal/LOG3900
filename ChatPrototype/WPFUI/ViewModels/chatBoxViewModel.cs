3ccfa3dc5778ea98f9ca48ba8e0ddfd7a185d1ae
Emilio Gagnon <emiliogagnonpro@gmail.com>
2020-01-31 01:14:11 -0500

    [EG] Messages.cs implemented in chatbox UI

--- ChatPrototype/WPFUI/ViewModels/chatBoxViewModel.cs
+++ ChatPrototype/WPFUI/ViewModels/chatBoxViewModel.cs
@@ -12,14 +12,41 @@ namespace WPFUI.ViewModels
     public class chatBoxViewModel: Screen
     {
         private IEventAggregator _events;
-        private List<MessageModel> _messages;
         private IUserData _userData;
+        private BindableCollection<MessageModel> _messages;
+        private string _currentMessage;
+
+        public string currentMessage
+        {
+            get { return _currentMessage; }
+            set { _currentMessage = value;
+                NotifyOfPropertyChange(() => currentMessage);}
+        }
+
+
+        public BindableCollection<MessageModel> messages
+        {
+            get { return _messages; }
+            set { _messages = value;
+                  NotifyOfPropertyChange(() => messages); }
+        }
+
+        public void sendMessage()
+        {
+            if (currentMessage != null & currentMessage != "")
+            {
+                messages.Add(new MessageModel(currentMessage, _userData.userName, DateTime.Now));
+            }
+
+        }
 
 
         public chatBoxViewModel(IUserData userdata, IEventAggregator events)
         {
             _events = events;
-            _messages = new List<MessageModel>();
+            _messages = new BindableCollection<MessageModel>();
+            messages.Add(new MessageModel("this is a test message", "Barack Obama", DateTime.Now));
+            messages.Add(new MessageModel("this is another test message", "Kobe", DateTime.Now));
             _userData = userdata;
         }
 
@@ -27,14 +54,20 @@ namespace WPFUI.ViewModels
         {
             get
             {
-                return $"Username: {_userData.userName} Server IP Adress: {_userData.ipAdress} ";
+                return $"Welcome {_userData.userName} ! Server IP: {_userData.ipAdress} ";
             }
         }
         public void disconnect()
         {
+            clearUserData();
+            _events.PublishOnUIThread(new DisconnectEvent());
+        }
+
+        private void clearUserData()
+        {
+            _messages = new BindableCollection<MessageModel>();
             _userData.ipAdress = "";
             _userData.userName = "";
-            _events.PublishOnUIThread(new DisconnectEvent());
         }
     }
 
