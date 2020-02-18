d4b65b89b94ace74b96f77912b0041abe0fa96e9
Hubert <hubert.michaud-dufour@hotmail.com>
2020-02-18 13:32:33 -0500

    HMD : httpController to test

--- Server/src/controller/http/httpController.ts
+++ Server/src/controller/http/httpController.ts
@@ -1,36 +1,28 @@
 import { JsonController, Get, Param, Post, Body } from "routing-controllers";
-import User from "../../models/user";
+import Profile from "../../models/profile";
 
 /**
  * HTTPController is used only to manage user database and game database. 
  */
-@JsonController("/user")
+@JsonController("/profile")
 export class HttpController {
 
     // Eventually 
 
-    @Post("/createUser")
-    createUser(@Body() user: User) {
-
-        // console.log(body)
-        // console.log(body.username);
-        // console.log(body.password);
-        // // this.room.addUser(body);
-        // // Create new user in database
-        // return body.username + " added";
+    @Post("/createProfile")
+    createUser(@Body() profile: Profile) {
+        console.log(profile);
     }
 
     @Get("/:userName")
     test(@Param("userName") userName: string) {
-        console.log("You sent : " + userName);
-        /*
-        let userRetrieved: User | undefined = this.room.getUser(userName);
-        if (userRetrieved) {
-            return userRetrieved;
-        } else {
-            return "User does not exist";
+        let profileRetrieved : Profile = {
+            firstname : "string",
+            lastname : "string",
+            username : "string",
+            password : "string"
         }
-        */
+        return profileRetrieved;
     }
 }
 
