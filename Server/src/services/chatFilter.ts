3e6a0d2f371807987ebc82d854cc9657a67ab75a
Hubert <hubert.michaud-dufour@hotmail.com>
2020-02-25 11:13:25 -0500

    HMD : commented error

--- Server/src/services/chatFilter.ts
+++ Server/src/services/chatFilter.ts
@@ -1,7 +1,7 @@
 const Filt = require('bad-words');
-const frenchlist = require('french-badwords-list');
+// const frenchlist = require('french-badwords-list');
 const filt = new Filt();
-filt.addWords(...frenchlist);
+// filt.addWords(...frenchlist);
 export default class ChatFilter {
 
     static filter(message: string): string {
