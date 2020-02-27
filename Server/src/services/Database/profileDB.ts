78f181642e96b78541487696da269658657f5d55
Hubert <hubert.michaud-dufour@hotmail.com>
2020-02-27 09:15:26 -0500

    HMD: small refactor indentations

--- Server/src/services/Database/profileDB.ts
+++ Server/src/services/Database/profileDB.ts
@@ -18,47 +18,45 @@ class ProfileDB {
 
     public async createProfile(profile: PrivateProfile): Promise<void> {
         await this.mongoDB.db("Profiles").collection("profiles")
-        .insertOne(profile)
-        .catch((err: any) => {
-            throw err;
-        });
+            .insertOne(profile)
+            .catch((err: any) => {
+                throw err;
+            });
     }
 
     public async getPrivateProfile(username: string): Promise<PrivateProfile | null> {
-        const privateProfile: PrivateProfile | null = await this.mongoDB.db("Profiles").collection("profiles")
-        .findOne({ username: { $eq: username } })
-
-        return privateProfile;
+        return await this.mongoDB.db("Profiles").collection("profiles")
+            .findOne({ username: { $eq: username } })
     }
 
     public async deleteProfile(username: string): Promise<void> {
         await this.mongoDB.db("Profiles").collection("profiles")
-        .deleteOne({ username: username });
+            .deleteOne({ username: username });
     }
 
     public async joinRoom(username: string, roomId: string): Promise<void> {
         await this.mongoDB.db("Profiles").collection("profiles")
-        .updateOne(
-            { username: username },
-            { $push: { rooms_joined : roomId } }
-        );
+            .updateOne(
+                { username: username },
+                { $push: { rooms_joined : roomId } }
+            );
     }
 
     public async leaveRoom(username: string, roomId: string): Promise<void> {
         await this.mongoDB.db("Profiles").collection("profiles")
-        .updateOne(
-            { username: username },
-            { $pull: { rooms_joined : roomId } }
-        );
+            .updateOne(
+                { username: username },
+                { $pull: { rooms_joined : roomId } }
+            );
     }
 
     public async updateProfile(profile: PrivateProfile): Promise<void>{
         await this.mongoDB.db("Profiles").collection("profiles")
-        .replaceOne(
-            {username: { $eq: profile.username}}, profile)
-        .catch((err: any) => {
-            throw err;
-        });
+            .replaceOne(
+                {username: { $eq: profile.username}}, profile)
+            .catch((err: any) => {
+                throw err;
+            });
     }
 }
 
