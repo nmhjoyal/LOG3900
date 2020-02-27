e90167daa954ba1c631ba412560be39a70ed6e27
Hubert <hubert.michaud-dufour@hotmail.com>
2020-02-27 01:10:31 -0500

    HMD: ProfileController uses interface feedback and removed getPublicUserInfos

--- Server/src/controller/http/profileController.ts
+++ Server/src/controller/http/profileController.ts
@@ -1,7 +1,8 @@
-import { JsonController, Get, Param, Post, Body, HttpError, Delete, NotFoundError, Put } from "routing-controllers";
+import { JsonController, Get, Param, Post, Body, Delete, Put } from "routing-controllers";
 import { profileDB } from "../../services/Database/profileDB";
-import PublicProfile from "../../models/publicProfile";
 import PrivateProfile from "../../models/privateProfile";
+import Feedback from "../../models/feedback";
+import Admin from "../../models/admin";
 
 /**
  * ProfileController is used to manage user profiles in the database. 
@@ -11,30 +12,55 @@ export class ProfileController {
    
     @Post("/create")
     public async createUser(@Body() profile: PrivateProfile) {
-        try {
-            await profileDB.createProfile(profile);
-        } catch {
-            throw new HttpError(400);
+
+        let feedback: Feedback = {
+            status: true,
+            log_message: "Profile " + profile.username + " created!"
+        };
+
+        if ((profile.username).toUpperCase() === (Admin.admin).toUpperCase()) {
+            feedback.status = false;
+            feedback.log_message = "Username Admin is reserved.";
+        } else {
+            try {
+                await profileDB.createProfile(profile);
+            } catch {
+                feedback.status = false;
+                feedback.log_message = "Could not create profile.";
+            }
         }
-        // Querry worked
-        return "Profile " + profile.username + " created!";
+        
+        return feedback;
     }
 
     @Delete("/:userName")
     public async deleteUserInfos(@Param("userName") userName: string) {
+
         await profileDB.deleteProfile(userName);
-        return "Profile " + userName + " deleted!"
+        let feedback: Feedback = {
+            status: true,
+            log_message: "Profile " + userName + " deleted!"
+        };
+        
+        return feedback;
     }
 
     @Put("/update")
     public async updateProfile(@Body() profile: PrivateProfile) {
+
+        let feedback: Feedback = {
+            status: true,
+            log_message: "Profile " + profile.username + " updated!"
+        };
+
         try {
             await profileDB.updateProfile(profile);
         } catch {
-            throw new HttpError(400);
+            feedback.status = false;
+            feedback.log_message = "Could not update profile.";
         }
-        // Querry worked
-        return "Profile " + profile.username + " updated!";
+        
+        return feedback;
     }
 
     @Get("/private/:userName")
@@ -42,13 +68,6 @@ export class ProfileController {
         const profileRetrieved: PrivateProfile | null = await profileDB.getPrivateProfile(userName);
         return profileRetrieved;
     }
-
-    // TEST DB : 
-    // @Get("/public/:userName")
-    // public async getPublicUserInfos(@Param("userName") userName: string) {
-    //     const profileRetrieved: PublicProfile | null = await profileDB.getPublicProfile(userName);
-    //     return profileRetrieved;
-    // }
 }
 
 // Ref : https://www.npmjs.com/package/routing-controllers
