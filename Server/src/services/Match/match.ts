86daf0fdaee159d61ff1609087c48af46d18475a
Hubert <hubert.michaud-dufour@hotmail.com>
2020-02-27 20:55:26 -0500

    HMD : methodes de base pour un Match non final

--- Server/src/services/Match/match.ts
+++ Server/src/services/Match/match.ts
@@ -1,10 +1,16 @@
 
 
 export default abstract class Match {
-    private matchId: number;
-    private players: string[];
+    // private matchId: number;
+    // private players: string[];
 
-    public constructor() {
+    public constructor() { }
 
-    }
+    // Eventuellement les methodes ne seront pas void, ils devront retourner le paquet a envoyer.
+    public abstract startMatch(): void;
+    public abstract endMatch(): void;
+    public abstract startRound(): void;
+    public abstract endRound(): void;
+    public abstract draw(): void;
+
 }
