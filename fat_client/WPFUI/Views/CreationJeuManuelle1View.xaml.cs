6c97822d0172def9bcbacc9b2b0ba0539b43768d
karima <karimasalem.ks@gmail.com>
2020-03-07 20:17:16 -0500

    [KS] Modification vue CreationManuelle1

--- fat_client/WPFUI/Views/CreationJeuManuelle1View.xaml.cs
+++ fat_client/WPFUI/Views/CreationJeuManuelle1View.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFUI.Models;
+using WPFUI.ViewModels;
 
 namespace WPFUI.Views
 {
@@ -20,9 +24,44 @@ namespace WPFUI.Views
     /// </summary>
     public partial class CreationJeuManuelle1View : UserControl
     {
+        IEventAggregator events;
+        ISocketHandler socketHandler;
         public CreationJeuManuelle1View()
         {
             InitializeComponent();
+            DataContext = new CreationJeuManuelle1ViewModel(events, socketHandler);
         }
+
+
+
+        // Pour la gestion de l'affichage de position du pointeur.
+        //private void surfaceDessin_MouseLeave(object sender, MouseEventArgs e) => textBlockPosition.Text = "";
+        private void surfaceDessin_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(surfaceDessin);
+            //textBlockPosition.Text = Math.Round(p.X) + ", " + Math.Round(p.Y) + "px";
+        }
+
+        //code pris de: https://stackoverflow.com/questions/8881865/saving-a-wpf-canvas-as-an-image
+        /*private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PART_Image.Source = RenderVisualService.RenderToPNGImageSource(PART_Canvas);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //saveFileDialog.FileName = txtEditor.Text;
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                RenderVisualService.RenderToPNGFile(PART_Canvas, saveFileDialog.FileName);
+            }
+
+        }
+
+        private void mainMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }*/
     }
 }
