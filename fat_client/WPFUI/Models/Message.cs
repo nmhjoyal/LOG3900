4611194d87d296bfe0b14d9cc07224d9b23b88d0
nmhjo <nicole.joyal@polymtl.ca>
2020-03-16 17:57:30 -0400

    NJ - Readd dev version of fat_client

---
+++ fat_client/WPFUI/Models/Message.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.Models
+{
+    public class Message
+    {
+
+        private User _author;
+
+        public User author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        private string _content;
+
+        public string content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        private double _date;
+
+        public double date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public Message(User author, string content, double timestamp)
+        {
+            _content = content;
+            _author = author;
+            _date = timestamp;
+        }
+
+    }
+}
