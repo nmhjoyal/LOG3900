a8486a1ac0ee70d106c338318d06a34345f39608
Zakari Rahal <zakari.rahal@polymtl.ca>
2020-03-18 01:07:55 -0400

    ZR: panoramic

--- Server/src/services/Drawing/virtualPlayer.ts
+++ Server/src/services/Drawing/virtualPlayer.ts
@@ -103,6 +103,7 @@ export class VirtualPlayer {
     private async panoramic(drawing: Stroke[], level: number): Promise<void> {
         // Gauche a droite pour le moment
         drawing.sort((a: Stroke, b: Stroke) => {
+            let aMin: number = a.StylusPoints.reduce((min, stylusPoint) => stylusPoint.X < min ? stylusPoint.X : min, a.StylusPoints[0].X);
             let bMin: number = b.StylusPoints.reduce((min, stylusPoint) => stylusPoint.X < min ? stylusPoint.X : min, b.StylusPoints[0].X);
             return aMin - bMin;
         });
