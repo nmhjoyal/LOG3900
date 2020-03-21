d3bb1090447e0fb1df951a58f7d69c9dfb1796cc
Hubert <hubert.michaud-dufour@hotmail.com>
2020-03-21 02:22:44 -0400

    HMD : correction to controller no JSON.stringify!

--- Server/src/controller/socket/matchController.ts
+++ Server/src/controller/socket/matchController.ts
@@ -1,11 +1,16 @@
 import { OnMessage, SocketController, MessageBody, ConnectedSocket, SocketIO } from "socket-controllers";
 import { serverHandler } from "../../services/serverHandler";
 import DrawPoint from "../../models/drawPoint";
-import { CreateMatch } from "../../models/match";
+import { CreateMatch, StartMatch } from "../../models/match";
 
 @SocketController()
 export default class MatchController {
 
+    /**
+     * 
+     * Draw test events
+     * 
+     */
     @OnMessage("connect_free_draw")
     public connect_free_draw(@ConnectedSocket() socket: SocketIO.Socket) {
         serverHandler.matchHandler.enterFreeDrawTestRoom(socket);
@@ -21,24 +26,33 @@ export default class MatchController {
         serverHandler.matchHandler.drawTest(io, socket, drawPoint);
     }
 
+    /**
+     * 
+     * Match events
+     * 
+     */
     @OnMessage("create_match")
     public async createMatch(@SocketIO() io: SocketIO.Server, @ConnectedSocket() socket: SocketIO.Socket, @MessageBody() createMatch: CreateMatch) {
-        socket.emit("match_created", await serverHandler.createMatch(io, socket, createMatch));
+        socket.emit("match_created", JSON.stringify(await serverHandler.createMatch(io, socket, createMatch)));
     }
 
     @OnMessage("join_match")
     public async joinMatch(@SocketIO() io: SocketIO.Server, @ConnectedSocket() socket: SocketIO.Socket, @MessageBody() matchId: string) {
-        socket.emit("match_joined", await serverHandler.joinMatch(io, socket, matchId));
+        socket.emit("match_joined", JSON.stringify(await serverHandler.joinMatch(io, socket, matchId)));
     }
 
     @OnMessage("leave_match")
     public async leaveMatch(@SocketIO() io: SocketIO.Server, @ConnectedSocket() socket: SocketIO.Socket, @MessageBody() matchId: string) {
-        socket.emit("match_left", await serverHandler.leaveMatch(io, socket, matchId));
+        socket.emit("match_left", JSON.stringify(await serverHandler.leaveMatch(io, socket, matchId)));
     }
 
     @OnMessage("get_matches")
     public getMatches(@ConnectedSocket() socket: SocketIO.Socket) {
-        socket.emit("update_matches", serverHandler.getAvailableMatches());
+        socket.emit("update_matches", JSON.stringify(serverHandler.getAvailableMatches()));
     }
 
+    @OnMessage("start_match")
+    public startMatch(@SocketIO() io: SocketIO.Server, @ConnectedSocket() socket: SocketIO.Socket, @MessageBody() startMatch: StartMatch) {
+        socket.emit("match_started", JSON.stringify(serverHandler.startMatch(io, socket, startMatch)));
+    }
 }
