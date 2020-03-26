59bf015eaf46371f33e3b24a71968cff1dc02dc2
Chargé Log1000 <amar.ghaly@polymtl.ca>
2020-03-26 13:43:02 -0400

    Modification des noms de certains fichiers

--- thin_client/app/src/main/java/com/example/thin_client/ui/game_mode/TabsGameMode.kt
+++ thin_client/app/src/main/java/com/example/thin_client/ui/game_mode/TabsGameMode.kt
@@ -6,12 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.thin_client.R
+import com.example.thin_client.ui.chat.ChatFromItem
+import com.example.thin_client.ui.chat.ChatUserJoined
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.games_list.*
 
-class Fragment1 : Fragment() {
+class TabsGameMode: Fragment() {
+
+    private val adapter = GroupAdapter<GroupieViewHolder>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        availablegames_rc.adapter = adapter
     }
 
 
@@ -22,6 +29,9 @@ class Fragment1 : Fragment() {
     ): View {
         return inflater.inflate(R.layout.games_list, container, false)
 
+    }
+    private fun showAvailableGames(text:String){
+      // adapter.add)
     }
 
 }
