e43717adbf11e28070f41e78481b803240c46d46
Chargé Log1000 <amar.ghaly@polymtl.ca>
2020-03-31 16:34:26 -0400

    retrieve des modif du waitingroom

--- thin_client/app/src/main/java/com/example/thin_client/ui/LobbyMenuFragment.kt
+++ thin_client/app/src/main/java/com/example/thin_client/ui/LobbyMenuFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 
 import com.example.thin_client.R
-import com.example.thin_client.ui.game_mode.CollabMatchMode
+
+import com.example.thin_client.server.SocketHandler
+import com.example.thin_client.ui.game_mode.MatchList
 import kotlinx.android.synthetic.main.lobby_menu_fragment.*
 
 class LobbyMenuFragment : Fragment() {
@@ -26,7 +29,21 @@ class LobbyMenuFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.lobby_menu_fragment, container, false)
+        val view:View = inflater.inflate(R.layout.lobby_menu_fragment, container, false)
+        view.isFocusableInTouchMode = true
+        view.requestFocus()
+        val joinMatch = view.findViewById<LinearLayout>(R.id.join_match)
+
+        joinMatch.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                val transaction = fragmentManager!!.beginTransaction()
+                val gamesList = MatchList()
+                transaction.replace(R.id.lobby_menu_container, gamesList)
+                transaction.addToBackStack(null)
+                transaction.commit()
+                SocketHandler.searchMatches()
+            }})
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- thin_client/app/src/main/java/com/example/thin_client/ui/game_mode/waitingroom/WaitingRoom.kt
+++ thin_client/app/src/main/java/com/example/thin_client/ui/game_mode/waitingroom/WaitingRoom.kt
@@ -1,9 +1,7 @@
 package com.example.thin_client.ui.game_mode.waitingroom
 
 
-
-
-
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,8 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.thin_client.R
-import com.example.thin_client.data.AvatarID
-import com.example.thin_client.ui.game_mode.MatchItem
+import com.example.thin_client.data.game.GameManager
+import com.example.thin_client.data.game.MatchMode
+import com.example.thin_client.data.server.SocketEvent
+import com.example.thin_client.server.SocketHandler
+import com.example.thin_client.ui.game_mode.Player
+import com.google.gson.Gson
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.collab_gameslist.*
@@ -21,11 +23,17 @@ import kotlinx.android.synthetic.main.fragment_waiting_room.*
 
 class WaitingRoom : Fragment() {
 
+    private var listener: IStartMatch? = null
+
+    interface IStartMatch{
+        fun startMatch()
+    }
     private val adapter = GroupAdapter<GroupieViewHolder>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         recyclerview_available_players.adapter = adapter
+        setUpSocketEvents()
 
     }
 
@@ -33,10 +41,36 @@ class WaitingRoom : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        when(GameManager.currentGameMode){
+            MatchMode.SOLO -> {}
+
+        }
 
         return inflater.inflate(R.layout.fragment_waiting_room, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is IStartMatch){
+            listener = context
+        } else {
+            throw RuntimeException()
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+    private fun setUpSocketEvents(){
+        if(SocketHandler.socket != null){
+            SocketHandler.socket?.on(SocketEvent.UPDATE_PLAYERS, ({ data ->
+                val players = Gson().fromJson(data.first().toString(),Array<Player>::class.java)
+                GameManager.playersList = players.toCollection(ArrayList())
+            }))
+        }
+
+    }
 
 
 }
