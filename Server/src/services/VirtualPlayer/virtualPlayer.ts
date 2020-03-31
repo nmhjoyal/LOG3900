44ebd899797c39d8ae77b8f4851035eef6c2b5b8
Hubert <hubert.michaud-dufour@hotmail.com>
2020-03-31 19:57:12 -0400

    HMD: finish last commit

---
+++ Server/src/services/VirtualPlayer/virtualPlayer.ts
@@ -0,0 +1,24 @@
+// import Player from "../../models/player";
+
+export default class VirtualPlayer {
+    //private vps: Player[];
+
+    public constructor() {
+        // this.vps = [
+        //     {
+        //         user : {
+        //             username: "",
+        //             avatar: ""
+        //         },
+        //         isHost: false,
+        //         isVirtual: true
+        //     }
+        // ];
+    }
+
+    // public create(): Player {
+    //     return {
+
+    //     }
+    // }
+}
