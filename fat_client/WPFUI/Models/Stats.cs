b9ffcf990f07d6fc36a87e7838ecd0cedbc484af
karima <karimasalem.ks@gmail.com>
2020-04-09 12:17:15 -0400

    KS - stats model

---
+++ fat_client/WPFUI/Models/Stats.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.Models
+{
+    public class StatsClient
+    {
+        public string username;
+        public int matchCount;
+        public double victoryPerc;
+        public int averageTime;
+        public int totalTime; 
+        public int bestSSS;
+        public int[] connections;
+        public int[] disconnections;
+        public MatchHistory[] matchesHistory; 
+    }
+
+    public class Stats
+    {
+        public string username;
+        public int bestSSS;
+        public int[] connections;
+        public int[] disconnections;
+        public MatchHistory[] matchesHistory;
+    }
+    public class MatchHistory
+    {
+        public int startTime;
+        public int endTime;
+        public string matchMode;
+        public string[] playerNames;
+        public Ranking winner;
+        public int myScore;
+    }
+}
